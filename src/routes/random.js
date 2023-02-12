fbfe6ad9cbf5b7a2a72df7db290faf4c975278a3
alvesds <quadratuz@icloud.com>
2023-02-11 21:18:16 -0300

    update random gif server

--- src/routes/random.js
+++ src/routes/random.js
@@ -1,16 +1,16 @@
-const gifs = './public/assets/gifs'
+const gifs = './public/assets/gifs';
 const random = require('express').Router();
-const fs = require('fs')
+const util = require('util');
+const fs = require('fs');
+const readdir = util.promisify(fs.readdir);
 
 random.get('/', async (req, res) => {
+    let files = await readdir(gifs);
+    let file = files[Math.floor(Math.random() * files.length)];
 
-    fs.readdir(gifs, (err, files) => {
-        files.forEach(file => {
-            console.log(file);
-        });
-    });
+    if (file === 'empty') return res.status(400).json({ message: "Dear developer, the 'empty' file must be deleted :c" });
 
-    return res.status(400).json({ message: "test" })
+    return res.sendFile(gifs + '/' + file, { root: "./" });
 });
 
 module.exports = random;
